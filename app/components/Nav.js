21094ac3c0040b0057929d35be1812907bb9cde3
arduino731 <arduino731@gmail.com>
2023-11-08 08:35:40 -0600

    Image

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,25 +1,93 @@
-// import headerLogo from '../wolfpawLeft1.png'; 
-
+import Image from 'next/image'
 
 const Nav = () => {
     return (
         <header>
             <nav>
+                <a href="/" >
+                    <Image 
+                        src="/images/wolfIcon.png" 
+                        className="p-10 bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    /> 
+                </a>
+
                 <h1 className="text-2x p-2 m-2 bg-slate-500 underline font-bold">logo here</h1>
                 <div className="p-2 m-2 flex justify-center ">
-                    <img src="./images/wolfIcon.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/wolf_paw_Right1.png" width="100" height="100" /> 
+                    <Image 
+                        src="/images/wolfIcon.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    /> 
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/wolf_paw_Right1.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
                 </div>
                 <div className="p-2 m-2 flex justify-center ">
-                    <img src="./images/wolfIcon.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/PawsLogo.png" width="100" height="100" /> 
-                    <img src="./images/wolfpawLeft1.png" width="100" height="100" /> 
-                    <img src="./images/hamburger.png" width="100" height="100" /> 
+                <Image 
+                        src="/images/wolfIcon.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    /> 
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/PawsLogo.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
+                    <Image 
+                        src="/images/wolf_paw_Right1.png" 
+                        className="bg-slate-500" 
+                        alt="logo"
+                        width={100} 
+                        height={100} 
+                    />
                 </div>
 
 
