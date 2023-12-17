ea8c9125995e5d0a123a6751930001478967c0fb
arduino731 <arduino731@gmail.com>
2023-12-17 15:35:41 -0600

    project desc

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -3,6 +3,7 @@
         <div>
             <div className="flex items-center justify-center">
                 <h1 className="text-5xl">PROJECTS</h1>
+                <p>adversarial search programming</p>
             </div>
         </div>
      );
