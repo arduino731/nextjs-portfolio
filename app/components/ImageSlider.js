05b340a3edaca752ed83320326188548d8ee5fe8
arduino731 <arduino731@gmail.com>
2024-02-13 00:47:47 -0600

    text position and absolute

--- app/components/ImageSlider.js
+++ app/components/ImageSlider.js
@@ -13,6 +13,30 @@ const ImageSlider = () => {
             });
     };
     
+    const findText =[
+        {
+            image: "images/fast.png",
+            title: "FAST", 
+            describe: "Fast load times and lag free interaction, my highest priority."
+        },{
+            image: "images/responsive.png",
+            title: "RESPONSIVE", 
+            describe: "My layouts will work on any device, big or small."
+        },{
+            image: "images/intuitive.png",
+            title: "INTUITIVE", 
+            describe: "Strong preference for easy to use, intuitive UX/UI."
+        },{
+            image: "images/dynamic.png",
+            title: "DYNAMIC", 
+            describe: "Websites don't have to be static, I love making pages come to life."
+        },{
+            image: "images/stack.png",
+            title: "FULL-STACK", 
+            describe: "Both user-facing and server-side aspects of web applications."
+        }
+
+    ]
 
     const findFileImage = ["images/dynamic.png", "images/fast.png", "images/intuitive.png", "images/responsive.png","images/stack.png"];
 
@@ -26,15 +50,38 @@ const ImageSlider = () => {
       right1: { x: "50%", scale: 0.7, zIndex: 3, rotate: 45},
     };
 
+    const textVariants = {
+        center: { opacity: 1},
+        left1: {  opacity: 0 },
+        left: {  opacity: 0},
+        right: {  opacity: 0 },
+        right1: {  opacity: 0},
+      };
+
   return (
     <div >
-        <h1>ImageSlider</h1>
+        <h1 className=" bg-slate-500 items-center justify-center flex text-5xl">About</h1>
         <div className="flex items-center flex-col justify-center bg-slate-500 h-screen ">
-        {findFileImage.map((image, index) => (
+        
+        {findText.map((item, index) => (
+        <motion.div
+          key={index}
+          initial="center"
+          animate={positions[positionIndexes[index]]}
+          variants={textVariants}
+          style={{width: "25vw",height: "50vh",marginBottom:"200px", position: "absolute"}}
+        >
+        <h2 className="text-3xl text-center">{item.title}</h2>
+        <p className="text-2xl text-center">{item.describe}</p>
+        </motion.div>
+
+        ))}
+
+        {findText.map((item, index) => (
             <motion.img 
             key={index}
-            src={image}
-            alt={image}
+            src={item.image}
+            alt={item.image}
             className="rounded-[12px] bg-red-600 border-solid border-2 border-black"
             initial="center"
             animate={positions[positionIndexes[index]]}
@@ -45,7 +92,7 @@ const ImageSlider = () => {
         ))}
             <div className="flex flex-row gap-3">
                 <button
-                className="text-white mt-[400px] bg-indigo-400 rounded-md py-2 px-4 z-10"
+                className="text-white mt-[500px] bg-indigo-400 rounded-md py-2 px-4 z-10"
                 onClick={handleNext}
                 >
                 Next
